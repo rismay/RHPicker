e9491fa805a86d47cde851656bb2d554ba1a3998
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2014-11-10 21:41:23 -0800

    Initial Commit

---
+++ RallyPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RallyPicker
+//
+//  Created by Cristian Monterroza on 11/10/14.
+//  Copyright (c) 2014 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
